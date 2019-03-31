d9ed9034079307aeee30e5f8c9bca0cbccfea712
CamilleMP <camille.patoleia@gmail.com>
2019-03-31 17:40:27 +0200

    Mon premier site

---
+++ php/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Alien Games</title>
+        <link rel="icon" href="../pictures/alien_rmn_icon.ico" />
+        <link rel="stylesheet" href="../css/reboot.css">
+        <link rel="stylesheet" href="../css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Pacifico" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Merienda:400,700" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <header>
+        <a href="index.php"> <img src="../pictures/game_machine.jpg" alt="Alien playing catch game">
+        <h1>Alien Games</h1></a>
+        </header>
+
+        <main>
+            <section id="left">
+            <ul>
+                <li><h2>Menu</h2></li>
+                <li><a class="menu" href="#sous_liste">Liste des Jeux</a></li>
+                <li><a class="menu" href="#">Marchandises</a></li>
+                <li><a class="menu" href="#">Forum</a></li>
+                <li><a class="menu" href="#">Recrutement</a></li>
+                <li><a class="menu" href="#">Contact</a></li>
+            </ul>    
+            
+                
+            </section>
+
+            <section id="middle">
+            
+            </section>
+
+            <section id="right">
+                <h2>News</h2>
+                <div id="supranews"> <div id="supernews"><div id="news">
+                <article class="news_article">
+                    <h3>Sortie d'Alien Geek </h3>
+                    <p id="dates">le 13/03/19</p>
+                    <p>Notre nouvel blockbuster est officiellement annoncé le 1er avril 2019. Preparez-vous pour de nouvelles aventures!</p>
+                </article>
+                <article class="news_article">
+                    <h3>Sortie de Alien Geek <span>le 13/03/19</span></h3>
+                    <p>Notre nouvel blockbuster est officiellement annoncé le 1er avril 2019. Preparez-vous pour de nouvelles aventures!</p>
+                </article>
+                
+                </div></div></div>
+
+                <h2>Newsletter</h2>
+                <p id="letter"><a id="newsletter" href="#">Inscrivez-vous</a> a notre newsletter pour rester informé/e des <strong>updates</strong> et <strong>news</strong> de nos jeux.</p>
+
+                <h2>Réseaux Sociaux</h2>
+                <div id="social_media">
+                    <a href="#"><img src="../pictures/facebook.png" alt="Facebook"> </a>
+                    <a href="#"><img src="../pictures/twitter.png" alt="Twitter"> </a>
+                    <a href="#"><img src="../pictures/instagram.png" alt="Instagram"> </a>
+                    <a href="#"><img src="../pictures/youtube.png" alt="Youtube"> </a>
+                    <a href="#"><img src="../pictures/linkedin.png" alt="Linkedin"> </a>
+                </div>
+
+            </section>
+        
+        </main>
+
+    </body>
+
+</html>
